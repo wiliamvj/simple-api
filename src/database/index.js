fb94c1ba992e2b57e455830b28946c50ee33e0f8
wiliamvj <wiliamjoaquim@gmail.com>
2020-04-30 21:10:10 -0300

    adit url mongo

--- src/database/index.js
+++ src/database/index.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  'mongodb+srv://admin:admin@omnistack09-4n2qz.mongodb.net/omnistack9?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect('URL Mongo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
