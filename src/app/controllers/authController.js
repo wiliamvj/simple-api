e2dd3f43217a8c9c0ef0b28e5d31bb6baecca5b7
wiliamvj <wiliamjoaquim@gmail.com>
2020-05-01 10:53:08 -0300

    create route generate token reset password

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -90,8 +90,37 @@ router.post('/forgot_password', async (req, res) => {
       }
     );
   } catch (err) {
-    console.log(err);
     res.status(400).send({ error: ' Error on forgot password, try again' });
+  }
+});
+
+router.post('/reset_password', async (req, res) => {
+  const { email, token, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email }).select(
+      '+passwordResetToken passwordResetExpired'
+    );
+
+    if (!user) return res.status(400).send({ error: 'User not found' });
+
+    if (token !== user.passwordResetToken)
+      return res.status(400).send({ error: 'Token invalid' });
+
+    const now = new Date();
+
+    if (now > user.passwordResetExpires)
+      return res
+        .status(400)
+        .send({ error: 'Token expired. generate a new one' });
+
+    user.password = password;
+
+    await user.save();
+
+    res.send();
+  } catch (err) {
+    res.status(400).send({ error: 'Cannot reset password, try again' });
   }
 });
 
